70db4ecf5c6cc5f2596a2b520c541a740a6d90df
Shorre McColman-Home <shorremccolman@gmail.com>
2017-08-05 02:19:51 -0600

    Upgraded to use Dijkstra’s

--- Assets/Agent.cs
+++ Assets/Agent.cs
@@ -2,6 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public struct PriorityNode {
+	public NavMeshNode node;
+	public float priority;
+
+	public PriorityNode(NavMeshNode node, float priority)
+	{
+		this.node = node;
+		this.priority = priority;
+	}
+}
+
 public class Agent : MonoBehaviour {
 
 	public NavMeshNode CurrentNode;
@@ -39,25 +50,45 @@ public class Agent : MonoBehaviour {
 			moving = false;
 	}
 
+	float CalculateCost(NavMeshNode current, NavMeshNode node) {
+		return Vector3.Distance (current.transform.position, node.transform.position);
+	}
+
 	void UpdatePath()
 	{
 		if (TargetNode == null)
 			return;
 
-		Queue<NavMeshNode> frontier = new Queue<NavMeshNode> ();
-		frontier.Enqueue (CurrentNode);
+		List<PriorityNode> frontier = new List<PriorityNode> ();
+		frontier.Add (new PriorityNode(CurrentNode,0f));
 
 		Dictionary<NavMeshNode, NavMeshNode> visitedDict = new Dictionary<NavMeshNode, NavMeshNode> ();
 		visitedDict.Add (CurrentNode, null);
 
+		Dictionary<NavMeshNode, float> costDict = new Dictionary<NavMeshNode, float> ();
+		costDict.Add (CurrentNode, 0);
+
 		bool found = false;
 
 		while(frontier.Count > 0 && !found) {
-			NavMeshNode current = frontier.Dequeue ();
+			NavMeshNode current = frontier [0].node;
+			frontier.RemoveAt (0);
+
 			foreach(NavMeshNode neighbor in current.neighbors) {
-				if(!visitedDict.ContainsKey(neighbor)) {
-					frontier.Enqueue (neighbor);
-					visitedDict.Add (neighbor, current);
+				float newCost = costDict [current] + CalculateCost(current,neighbor);
+
+				if(!costDict.ContainsKey(neighbor) || newCost < costDict[neighbor]) {
+					costDict [neighbor] = newCost;
+
+					frontier.Add (new PriorityNode(neighbor,newCost));
+					frontier.Sort (delegate(PriorityNode x, PriorityNode y) {
+						return x.priority.CompareTo(y.priority);
+					});
+
+					if (visitedDict.ContainsKey (neighbor))
+						visitedDict [neighbor] = current;
+					else
+						visitedDict.Add (neighbor, current);
 				}
 
 				if(neighbor == TargetNode) {
